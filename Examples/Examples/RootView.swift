e9766fd4c8d21387862c066d96c8d8e7493615d8
Michael Housh <mhoush@houshhomeenergy.com>
2024-04-24 19:21:41 -0400

    feat: Updates to latest versions of TCA

--- Examples/Examples/RootView.swift
+++ Examples/Examples/RootView.swift
@@ -3,49 +3,40 @@ import SupabaseClientDependencies
 import Supabase
 import SwiftUI
 
+@Reducer
 struct RootFeature: Reducer {
+  
+//  @Reducer(state: .equatable, action: .equatable)
+//  enum Destination {
+//    case loggedIn(TodoListFeature)
+//    case loggedOut(AuthFeature)
+//  }
+  
+  @ObservableState
   struct State: Equatable {
-    var destination: Destination.State = .loggedOut(.init())
+    @Presents var destination: Destination.State? = .loggedOut(.init())
   }
   
   enum Action: Equatable {
-    case destination(Destination.Action)
+    case destination(PresentationAction<Destination.Action>)
     case receiveAuthEvent(AuthChangeEvent)
     case signOutButtonTapped
     case task
   }
 
-  struct Destination: Reducer  {
-    enum State: Equatable {
-      case loggedIn(TodoListFeature.State)
-      case loggedOut(AuthFeature.State)
-    }
-
-    enum Action: Equatable {
-      case loggedIn(TodoListFeature.Action)
-      case loggedOut(AuthFeature.Action)
-    }
-
-    var body: some ReducerOf<Self> {
-      Scope(state: /State.loggedIn, action: /Action.loggedIn) {
-        TodoListFeature()
-      }
-      Scope(state: /State.loggedOut, action: /Action.loggedOut) {
-        AuthFeature()
-      }
-    }
+  @Reducer(state: .equatable, action: .equatable)
+  enum Destination {
+    case loggedIn(TodoListFeature)
+    case loggedOut(AuthFeature)
   }
 
   @Dependency(\.supabaseClient.auth) var auth;
   
   var body: some ReducerOf<Self> {
-    Scope(state: \.destination, action: /Action.destination) {
-      Destination()
-    }
     Reduce { state, action in
       switch action {
 
-      case .destination(.loggedOut(.receiveSession(.success(_)))):
+      case .destination(.presented(.loggedOut(.receiveSession(.success(_))))):
         state.destination = .loggedIn(.init())
         return .none
 
@@ -72,43 +63,36 @@ struct RootFeature: Reducer {
         }
       }
     }
+    .ifLet(\.$destination, action: \.destination)
   }
 }
 
 struct RootView: View {
-  let store: StoreOf<RootFeature>
+  @Perception.Bindable var store: StoreOf<RootFeature>
   
   var body: some View {
-    WithViewStore(store, observe: { _ in true }) { viewStore in
+    WithPerceptionTracking {
       NavigationStack {
-        SwitchStore(
-          store.scope(state: \.destination, action: { .destination($0) })
-        ) { state in
-          switch state {
-          case .loggedIn:
-            CaseLet(
-              /RootFeature.Destination.State.loggedIn,
-               action: RootFeature.Destination.Action.loggedIn
-            ) { store in
-              TodoListView.init(store: store)
-                .toolbar {
-                  ToolbarItem(placement: .cancellationAction) {
-                    Button("Sign Out") {
-                      viewStore.send(.signOutButtonTapped)
-                    }
+        Text("Loading...")
+          .navigationDestination(
+            item: $store.scope(state: \.destination?.loggedIn, action: \.destination.loggedIn)
+          ) { store in
+            TodoListView(store: store)
+              .navigationBarBackButtonHidden()
+              .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                  Button("Sign Out") {
+                    self.store.send(.signOutButtonTapped)
                   }
                 }
-            }
-          case .loggedOut:
-            CaseLet(
-              /RootFeature.Destination.State.loggedOut,
-               action: RootFeature.Destination.Action.loggedOut,
-               then: AuthView.init(store:)
-            )
+              }
           }
-        }
+          .navigationDestination(
+            item: $store.scope(state: \.destination?.loggedOut, action: \.destination.loggedOut),
+            destination: AuthView.init(store:)
+          )
       }
-      .task { await viewStore.send(.task).finish() }
+      .task { await store.send(.task).finish() }
     }
   }
 
