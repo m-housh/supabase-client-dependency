edd8e9d08452411febb243cfa6aba82283ea9fd8
Michael Housh <mhoush@houshhomeenergy.com>
2024-05-06 20:08:18 -0400

    feat: Updates to load env

--- Examples/Examples/SecretConstants.swift
+++ Examples/Examples/SecretConstants.swift
@@ -1,5 +1,3 @@
-import Foundation
-
 enum Secrets {
   static let supabaseUrl = URL(string: "http://localhost:54321")!
   static let supabaseAnonKey = "my.supabase.anon.key"
--- scripts/load_env.sh
+++ scripts/load_env.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 env_file="${1:-.env}"
 
@@ -8,11 +8,17 @@ if [ ! -f "$env_file" ]; then
 fi
 
 while IFS="" read -r line; do
-    line="$(echo "${line%%}" | xargs)"
-    if [ -n "$line" ]; then
-        export "$line" || {
-            echo "Error exporting: $line" >&2
-            exit 1
-        }
+    # Skip lines that begin with a '#' symbol.
+    if [[ "$line" =~ \#.* ]]; then
+        #echo "skipping line: $line"
+        continue
+    else
+        line="${line%%}"
+        if [ -n "$line" ]; then
+            export "$line" || {
+                echo "Error exporting: $line" >&2
+                exit 1
+            }
+        fi
     fi
 done <"$env_file"
